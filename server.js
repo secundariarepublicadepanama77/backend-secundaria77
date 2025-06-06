5c71c101c64667eb2771919852f3f3f3a623bad3
secundariarepublicadepanama77 <atecnologias.77@gmail.com>
2025-06-05 23:34:08 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+
 const { createClient } = require("@supabase/supabase-js");
 
-// Claves de Supabase (cópialas desde tu dashboard)
-const SUPABASE_URL = "https://ohjdzyzckqeepbnezcem.supabase.co";
-const SUPABASE_KEY = "TU_CLAVE_ANON"; // 🔁 reemplaza con la anon key que copiaste
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
+const supabase = createClient(
+  "https://TU-PROJECT-URL.supabase.co",   // remplaza por tu real
+  "TU-CLAVE-ANON"
+);
 
 const app = express();
 const PORT = 3000;
